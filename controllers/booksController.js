193ae813597a97775263a17babaa376a268d3502
Michelle McConville <mchelmconvl@gmail.com>
2021-02-05 16:53:57 -0600

    added create f(x)

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -14,5 +14,10 @@ module.exports = {
       .them(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-
+  create: function(req, res) {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
 }
