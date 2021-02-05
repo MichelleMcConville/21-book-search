02afbf91fc167f607070696d254d2262ea91bfb9
Michelle McConville <mchelmconvl@gmail.com>
2021-02-05 16:51:27 -0600

    added findById f(x)

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -8,4 +8,11 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findById: function (req, res) {
+    db.Book
+      .findById(req.params.id)
+      .them(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
 }
