dd3bb5855345c69ad0a48c620fe12d5a6ded4165
Michelle McConville <mchelmconvl@gmail.com>
2021-02-05 17:36:01 -0600

    added router.route / findAll, create

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,2 +1,4 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
+
+router.route("/").get(booksController.findAll).post(booksController.create);
