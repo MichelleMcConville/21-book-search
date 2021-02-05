4bc938c43a0e1143bc35cb23d65bf15050a30c4a
Michelle McConville <mchelmconvl@gmail.com>
2021-02-05 17:39:01 -0600

    added module.exports

--- routes/api/books.js
+++ routes/api/books.js
@@ -4,3 +4,5 @@ const booksController = require("../../controllers/booksController");
 router.route("/").get(booksController.findAll).post(booksController.create);
 
 router.route("/:id").delete(booksController.remove);
+
+module.exports = router;
