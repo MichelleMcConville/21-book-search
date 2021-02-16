e86ac388a5414fbbeccb31be7b1482e8635553fd
Michelle McConville <mchelmconvl@gmail.com>
2021-02-15 20:58:04 -0600

    re-coded page files

---
+++ client/public/src/pages/NoMatch.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class NoMatch extends Component {
+  render() {
+    return (
+      <h1>404 Page Not Found<span role="img" aria-label="Face With Rolling Eyes Emoji">🙄</span></h1>
+
+    );
+  }
+}
+
+export default NoMatch;
