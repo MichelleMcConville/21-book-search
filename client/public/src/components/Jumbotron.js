7a8d753707267b701dd9255289862c74825dac1c
Michelle McConville <mchelmconvl@gmail.com>
2021-02-15 20:57:35 -0600

    re-coded component files

---
+++ client/public/src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Jumbo from "react-bootstrap/Jumbotron";
+
+const Header = (props) => (
+  <Jumbo fluid className="text-center">
+    <Container>
+      <h1>Google (React) Books Search</h1>
+      <p>Search & save books of interest!</p>
+    </Container>
+  </Jumbo>
+);
+
+export default Header;
